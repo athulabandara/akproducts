27f24d75bf7993d39443f0756b8d8f72f8b4667e
athulabandara <75322924+athulabandara@users.noreply.github.com>
2023-03-08 16:18:34 +0530

    Add files via upload

--- sendMassges.php
+++ sendMassges.php
@@ -1,4 +1,9 @@
 <?php
-    $name =$_POST['Fname']
-    $name =$_POST['Fname']
-    $name =$_POST['Fname']
+    include_once "dbc.php";
+    $name =$_POST['Fname'];
+    $email =$_POST['Email'];
+    $massages =$_POST['massage'];
+
+    $sql="INSERT INTO regsheet(name,email,massages) VALUES ('$name','$email','$massages');";
+    $results =mysqli_query($con,$sql);
+    header("Location:index.html?Successfully_added_massage_to_the_database");
